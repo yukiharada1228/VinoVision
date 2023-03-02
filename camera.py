465a1bc1bd916731e399d58e916c4d1c49fadafc
yukiharada1228 <yukiharada1228@gmail.com>
2023-03-02 17:59:27 +0900

    顔検出の機能を追加しました

--- camera.py
+++ camera.py
@@ -1,41 +1,42 @@
 import logging
+
 import cv2 as cv
 
 # ロギングの設定
 logger = logging.getLogger(__name__)
 
+
 class Camera(object):
     def __init__(self, device):
-         # カメラデバイスを開く
+        # カメラデバイスを開く
         self.cap = cv.VideoCapture(device)
         if not self.cap.isOpened():
             # 開けなかった場合はエラー処理
-            logger.error('カメラを開けませんでした。device: %s', device)
-            raise RuntimeError('カメラを開けませんでした。device: %s' % device)
+            logger.error("カメラを開けませんでした。device: %s", device)
+            raise RuntimeError("カメラを開けませんでした。device: %s" % device)
         else:
             # 開けた場合はデバッグ情報を出力
-            logger.debug('カメラを正常に開きました。device: %s', device)
+            logger.debug("カメラを正常に開きました。device: %s", device)
 
     def read(self):
-        ret, frame = self.cap.read() 
-        if not ret: 
+        ret, frame = self.cap.read()
+        if not ret:
             # 読み取れなかった場合はエラー処理
-            logger.error('カメラからフレームを読み取れませんでした。')
+            logger.error("カメラからフレームを読み取れませんでした。")
         return ret, frame
 
-    def release(self): 
+    def release(self):
         # カメラデバイスのリソース解放処理
-        self.cap.release() 
-        logger.debug('カメラを正常に解放しました。')
+        self.cap.release()
+        logger.debug("カメラを正常に解放しました。")
 
     def __del__(self):
-        if hasattr(self, 'cap') and self.cap.isOpened(): 
+        if hasattr(self, "cap") and self.cap.isOpened():
             # カメラデバイスが開いている場合は解放する
             self.release()
 
 
 if __name__ == "__main__":
-    
     DEVICE = 0
     DELAY = 1
     KEYCODE_ESC = 27
@@ -45,6 +46,7 @@ if __name__ == "__main__":
         while camera.cap.isOpened():
             _, frame = camera.read()
             cv.imshow("frame", frame)
+            cv.imwrite("frame.jpg", frame)
             logger.debug({"frame.shape": frame.shape})
             key = cv.waitKey(DELAY)
             if key == KEYCODE_ESC:
--- model.py
+++ model.py
@@ -1,62 +1,174 @@
 import logging
 
-import numpy as np
 import cv2 as cv
-
+import numpy as np
 from openvino.inference_engine import IECore
+
+from camera import Camera
 
 # ロギングの設定
 logger = logging.getLogger(__name__)
 
+
 class Model(object):
-
-    def __init__(self, ie_core, model_path, device_name='CPU', num_requests=0):
-
+    def __init__(self, ie_core, model_path, device_name="CPU", num_requests=0):
         # モデルの読み込み
-        net = ie_core.read_network(model_path+'.xml',model_path+'.bin')
-        
+        net = ie_core.read_network(model_path + ".xml", model_path + ".bin")
+
         # モデルをデバイス上で実行するための準備
-        self.exec_net = ie_core.load_network(network=net,
-                                             device_name=device_name,
-                                             num_requests=num_requests)
-        
+        self.exec_net = ie_core.load_network(
+            network=net, device_name=device_name, num_requests=num_requests
+        )
+
         # 入力名・出力名・入力サイズ・出力サイズを設定
         self.input_name = next(iter(net.input_info))
         self.output_name = next(iter(net.outputs))
         self.input_size = net.input_info[self.input_name].input_data.shape
-        self.output_size = self.exec_net.requests[0].output_blobs[self.output_name].buffer.shape
+        self.output_size = (
+            self.exec_net.requests[0].output_blobs[self.output_name].buffer.shape
+        )
 
     def prepare_frame(self, frame):
-        # 入力サイズにリサイズし、画像のチャンネルを先頭に配置
+        # 入力サイズにリサイズし、画像のチャンネルを先頭に配置，4次元配列に変換
         _, _, h, w = self.input_size
-        input_frame = cv.resize(frame, (h, w))
-        input_frame = input_frame.transpose((2,0,1))
-        
-        # 入力データを4次元配列に変換
-        input_frame = np.expand_dims(input_frame, axis=0)
-        
+        input_frame = cv.resize(frame, (h, w)).transpose((2, 0, 1))[np.newaxis]
+
         # ログ出力
-        logger.debug({'action': 'prepare_frame', 
-                      'input_size': (h,w), 
-                      'input_frame.shape': input_frame.shape})
-        
+        logger.debug(
+            {
+                "action": "prepare_frame",
+                "input_size": (h, w),
+                "input_frame.shape": input_frame.shape,
+            }
+        )
+
         return input_frame
 
     def infer(self, data):
         # 入力データを辞書形式に変換
         input_data = {self.input_name: data}
-        
+
         # 推論を実行し、推論結果を取得
         infer_result = self.exec_net.infer(input_data)[self.output_name]
-        
+
         # ログ出力
-        logger.debug({'action': 'infer', 
-                      'input_data.shape': input_data[self.input_name].shape, 
-                      'infer_result.shape': infer_result.shape})
-        
+        logger.debug(
+            {
+                "action": "infer",
+                "input_data.shape": input_data[self.input_name].shape,
+                "infer_result.shape": infer_result.shape,
+            }
+        )
+
         return infer_result
 
-class FacialDetectionModel(Model):
 
-    def __init__(self, ie_core, model_path, device_name='CPU', num_requests=0):
-        super(FacialDetectionModel, self).__init__(ie_core=ie_core, model_path=model_path, device_name=device_name, num_requests=num_requests)
+class FacialDetectionModel(Model):
+    def __init__(self, ie_core, model_path, device_name="CPU", num_requests=0):
+        super(FacialDetectionModel, self).__init__(
+            ie_core=ie_core,
+            model_path=model_path,
+            device_name=device_name,
+            num_requests=num_requests,
+        )
+
+    def prepare_data(self, input, frame, confidence=0.5):
+        data_array = []
+        index_conf = 2
+        index_xmin = 3
+        index_ymin = 4
+        index_xman = 5
+        index_ymax = 6
+        index_x = 1
+        index_y = 0
+        for data in np.squeeze(input):
+            conf = data[index_conf]
+            xmin = max(0, int(data[index_xmin] * frame.shape[index_x]))
+            ymin = max(0, int(data[index_ymin] * frame.shape[index_y]))
+            xmax = min(
+                int(data[index_xman] * frame.shape[index_x]), frame.shape[index_x]
+            )
+            ymax = min(
+                int(data[index_ymax] * frame.shape[index_y]), frame.shape[index_y]
+            )
+            if conf > confidence:
+                area = (xmax - xmin) * (ymax - ymin)
+                data = {
+                    "xmin": xmin,
+                    "ymin": ymin,
+                    "xmax": xmax,
+                    "ymax": ymax,
+                    "area": area,
+                }
+                data_array.append(data)
+                data_array.sort(key=lambda face: face["area"], reverse=True)
+        logger.debug(
+            {
+                "action": "prepare_data",
+                "input.shape": input.shape,
+                "data_array": data_array,
+            }
+        )
+        return data_array
+
+    def draw(self, data_array, frame):
+        face_frame = frame.copy()
+        blue = (255, 0, 0)
+        for data in data_array:
+            cv.rectangle(
+                face_frame,
+                (int(data["xmin"]), int(data["ymin"])),
+                (int(data["xmax"]), int(data["ymax"])),
+                color=blue,
+                thickness=3,
+            )
+            logger.debug(
+                {
+                    "action": "draw",
+                    "face_frame.shape": face_frame.shape,
+                }
+            )
+        return face_frame
+
+
+if __name__ == "__main__":
+    import subprocess
+    import sys
+    from pathlib import Path
+
+    logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+
+    DEVICE = 0
+    DELAY = 1
+    KEYCODE_ESC = 27
+    XMIN = 0
+    YMIN = 1
+    XMAX = 2
+    YMAX = 3
+    IECORE = IECore()
+    CMD_OMZ = "omz_downloader --name face-detection-retail-0005"
+    PROJECT_ROOT = Path(__file__).resolve().parent
+    MODEL_DIR = PROJECT_ROOT / "intel" / "face-detection-retail-0005"
+    MODEL_PATH = str(MODEL_DIR / "FP16/face-detection-retail-0005")
+    if not MODEL_DIR.exists():
+        subprocess.call(CMD_OMZ.split(" "), cwd=str(PROJECT_ROOT))
+
+    camera = Camera(DEVICE)
+    face_detector = FacialDetectionModel(IECORE, MODEL_PATH)
+    try:
+        while camera.cap.isOpened():
+            _, frame = camera.read()
+            input_frame = face_detector.prepare_frame(frame)
+            infer_result = face_detector.infer(input_frame)
+            data_array = face_detector.prepare_data(infer_result, frame)
+            face_frame = face_detector.draw(data_array, frame)
+            cv.imshow("face_frame", face_frame)
+            logger.debug({"face_frame.shape": face_frame.shape})
+            key = cv.waitKey(DELAY)
+            if key == KEYCODE_ESC:
+                raise (KeyboardInterrupt)
+    except KeyboardInterrupt as ex:
+        logger.warning({"ex": ex})
+    finally:
+        del camera
+        cv.destroyAllWindows()
